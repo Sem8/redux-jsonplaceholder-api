59da588f181e4a08a6244bc5b3f96fd3d61eeb3a
Sem8 <mssemmi8@gmail.com>
2019-05-24 18:28:32 -0400

    form added for posting new posts in react

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Posts from './components/Posts';
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
     <div className="App">
-     
-        <Posts />
-      
+      <PostForm />
+      <hr />
+      <Posts />
     </div>
   );
 }
