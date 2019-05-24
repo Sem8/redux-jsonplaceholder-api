7c82877fb853bf3218137ad50aea69acfbab34ae
Sem8 <mssemmi8@gmail.com>
2019-05-24 18:39:00 -0400

    form functionality added for posting new posts in react pre-redux

--- reduxexample/src/components/PostForm.js
+++ reduxexample/src/components/PostForm.js
@@ -5,14 +5,32 @@ class PostForm extends Component {
     super(props);
     this.state = {
       title: "",
-      body: "",
+      body: ""
     };
   }
 
-  onChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value });
-  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
+  onSubmit = e => {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("http://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
 
   render() {
     return (
