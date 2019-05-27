8202fa4da485be7b0e77e7a85c254df46d57c8d5
Sem8 <mssemmi8@gmail.com>
2019-05-27 17:39:06 -0400

    fetching posts completely refactored to redux

--- reduxexample/src/reducers/postReducer.js
+++ reduxexample/src/reducers/postReducer.js
@@ -1,13 +1,18 @@
-import { FETCH_POSTS, NEW_POST } from '../actions/types';
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
 
 const initialState = {
-    items: [],
-    item: {}
+  items: [],
+  item: {}
 };
 
 export default function(state = initialState, action) {
-    switch(action.type) {
-        default: 
-        return state;
-    }
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload
+      };
+    default:
+      return state;
+  }
 }
